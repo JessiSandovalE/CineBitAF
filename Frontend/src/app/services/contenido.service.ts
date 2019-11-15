76f3d635da269b238bf070492123a71876ff0143
jessi <jessi.sandoval93@gmail.com>
2019-11-14 20:12:00 -0500

    Presentación sin Login

--- Frontend/src/app/services/contenido.service.ts
+++ Frontend/src/app/services/contenido.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient, HttpHeaders, HttpErrorResponse } from '@angular/common/http';
 import { Observable, of } from 'rxjs';
 import { map, catchError, tap } from 'rxjs/operators';
-import { contenido } from "src/modulo/contenido.module";
+import { contenido } from "src/app/module/contenido.module";
 
 @Injectable({
   providedIn: "root"
